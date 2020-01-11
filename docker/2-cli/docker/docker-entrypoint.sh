d98560f428aaf0505e6a9c477176e68f8649ce6a
zhenik <nikita.zhevnitskiy@gmail.com>
2020-01-11 13:05:14 +0100

    done cli-docker

--- docker/2-cli/docker/docker-entrypoint.sh
+++ docker/2-cli/docker/docker-entrypoint.sh
@@ -5,12 +5,12 @@ echo "Configuration check"
 echo "I am in directory"; pwd
 
 # sleep for some time
-if [[ -z "$SLEEP_TIME" ]]; then
+if [[ -z ${SLEEP_TIME} ]]; then
   SLEEP_TIME=25                     # default value, because $SLEEP_TIME is empty string
   echo "$SLEEP_TIME"                # print value of env variable
 else
   echo "$SLEEP_TIME"
 fi
-
-echo "Application started"; sleep 40                # running program simulation (40 seconds)
+echo "Sleep before start $SLEEP_TIME"                  # sleep before start :) just a simulation
+echo "Application started"; sleep 40000                # running program simulation (40_000 seconds)
 echo "I am done"
